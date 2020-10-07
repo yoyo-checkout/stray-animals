24a1cda0677ffc7c7db75d9478f0ebb766071641
yoyo-checkout <stem597sag445@gmail.com>
2020-10-07 11:04:33 +0800

    init app bar

---
+++ components/layouts/AppBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar
+    color="blue darken-2"
+    elevate-on-scroll
+    app
+  >
+    <v-app-bar-nav-icon />
+    <v-toolbar-title>Logo</v-toolbar-title>
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      app-bar
-    </v-app-bar>
+    <AppBar />
 
     <!-- Sizes your content based upon application components -->
     <v-main>
@@ -23,6 +21,16 @@
     </v-bottom-navigation>
   </v-app>
 </template>
+
+<script>
+import AppBar from '@/components/layouts/AppBar.vue';
+
+export default {
+  components: {
+    AppBar,
+  },
+};
+</script>
 
 <style>
 html {
@@ -49,34 +57,5 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
