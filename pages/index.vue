541e483dfd6ac0e93efa1dffc503f1058313efb5
zongyo <stem597sag445@gmail.com>
2020-10-11 21:13:15 +0800

    move info maps to static dir

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,16 @@
           >
             <v-chip-group class="chip-group">
               <v-chip v-if="animal.animal_sex === 'F'" color="pink lighten-4">
-                {{ sexMap[animal.animal_sex] }}
+                {{ maps.sex[animal.animal_sex] }}
               </v-chip>
               <v-chip v-if="animal.animal_sex === 'M'" color="blue lighten-4">
-                {{ sexMap[animal.animal_sex] }}
+                {{ maps.sex[animal.animal_sex] }}
               </v-chip>
               <v-chip v-if="animal.animal_bodytype" color="teal lighten-4">
-                {{ bodyTypeMap[animal.animal_bodytype] }}
+                {{ maps.bodyType[animal.animal_bodytype] }}
               </v-chip>
               <v-chip v-if="animal.animal_age" color="brown lighten-4">
-                {{ ageMap[animal.animal_age] }}
+                {{ maps.age[animal.animal_age] }}
               </v-chip>
               <v-chip v-if="animal.animal_colour">
                 {{ animal.animal_colour }}
@@ -42,7 +42,7 @@
                 class="blue--text map-link"
                 @click="openGoogleMap(animal.shelter_name)"
               >
-                {{ areaMap[animal.animal_area_pkid] }}
+                {{ maps.area[animal.animal_area_pkid] }}
               </span>
             </div>
           </v-card-text>
@@ -53,6 +53,8 @@
 </template>
 
 <script>
+import * as infoMaps from '@/static/infoMaps';
+
 export default {
   layout: 'default',
   // async fetch ({ $axios }) {
@@ -62,78 +64,7 @@ export default {
   // },
   data () {
     return {
-      sexMap: {
-        M: '公',
-        F: '母',
-      },
-      bodyTypeMap: {
-        SMALL: '小型',
-        MEDIUM: '中型',
-        BIG: '大型',
-      },
-      ageMap: {
-        CHILD: '幼年',
-        ADULT: '成年',
-      },
-      areaMap: {
-        2: '台北市',
-        3: '新北市',
-        4: '基隆市',
-        5: '宜蘭縣',
-        6: '桃園縣',
-        7: '新竹縣',
-        8: '新竹市',
-        9: '苗栗縣',
-        10: '台中市',
-        11: '彰化縣',
-        12: '南投縣',
-        13: '雲林縣',
-        14: '嘉義縣',
-        15: '嘉義市',
-        16: '台南市',
-        17: '高雄市',
-        18: '屏東縣',
-        19: '花蓮縣',
-        20: '台東縣',
-        21: '澎湖縣',
-        22: '金門縣',
-        23: '連江縣',
-      },
-      shelterMap: {
-        48: '基隆市寵物銀行',
-        49: '臺北市動物之家',
-        50: '新北市板橋公立動物之家',
-        51: '新北市新店區公立動物之家',
-        53: '新北市中和區公立動物之家',
-        55: '新北市淡水區公立動物之家',
-        56: '新北市瑞芳區公立動物之家',
-        58: '新北市五股區公立動物之家',
-        59: '新北市八里區公立動物之家',
-        60: '新北市三芝區公立動物之家',
-        61: '桃園市動物保護教育園區',
-        62: '新竹市動物收容所',
-        63: '新竹縣動物收容所',
-        67: '臺中市動物之家南屯園區',
-        68: '臺中市動物之家后里園區',
-        69: '彰化縣流浪狗中途之家',
-        70: '南投縣公立動物收容所',
-        71: '嘉義市流浪犬收容中心',
-        72: '嘉義縣流浪犬中途之家',
-        73: '臺南市動物之家灣裡站',
-        74: '臺南市動物之家善化站',
-        75: '高雄市壽山動物保護教育園區',
-        76: '高雄市燕巢動物保護關愛園區',
-        77: '屏東縣流浪動物收容所',
-        78: '宜蘭縣流浪動物中途之家',
-        79: '花蓮縣流浪犬中途之家',
-        80: '台東縣動物收容中心',
-        81: '連江縣流浪犬收容中心',
-        82: '金門縣動物收容中心',
-        83: '澎湖縣流浪動物收容中心',
-        89: '雲林縣流浪動物收容所',
-        92: '新北市政府動物保護防疫處',
-        96: '苗栗縣生態保育教育中心',
-      },
+      maps: { ...infoMaps },
       animals: [
         {
           animal_area_pkid: 16, // 縣市 id
