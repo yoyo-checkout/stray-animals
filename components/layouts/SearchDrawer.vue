f5600bd435f7de5c22d9329de1e227df5403015a
yoyo-checkout <stem597sag445@gmail.com>
2020-10-16 15:30:34 +0800

    update search drawer

--- components/layouts/SearchDrawer.vue
+++ components/layouts/SearchDrawer.vue
@@ -11,14 +11,159 @@
     stateless
     temporary
   >
-    this is search drawer
+    <v-list>
+      <!-- 動物類型 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            動物類型
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group class="chip-group">
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                狗
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                貓
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- 動物性別 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            動物性別
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group class="chip-group">
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                公
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                母
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- 動物年紀 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            動物年紀
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group class="chip-group">
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                幼年
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                成年
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- 動物體型 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            動物體型
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group class="chip-group">
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                小型
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                中型
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                大型
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- 是否絕育 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            是否絕育
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group class="chip-group">
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                是
+              </v-chip>
+              <v-chip color="pink lighten-4">
+                否
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- 動物所在地 -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            動物所在地
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <v-chip-group
+              class="chip-group"
+              column
+            >
+              <v-chip color="pink lighten-4">
+                全部
+              </v-chip>
+              <v-chip v-for="(area, i) in areas" :key="i" color="pink lighten-4">
+                {{ area }}
+              </v-chip>
+            </v-chip-group>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-btn class="submit-btn" color="info">
+      搜尋
+    </v-btn>
   </v-navigation-drawer>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import { area } from '@/static/infoMaps';
 
 export default {
+  data() {
+    return {
+      areas: area,
+    };
+  },
   computed: {
     ...mapState('Global', {
       isSearchDrawerOpen: state => state.isSearchDrawerOpen,
@@ -32,5 +177,16 @@ export default {
   &-navigation-drawer {
     top: 56px !important;
   }
+
+  &-list {
+    margin: 0 0 50px;
+  }
+}
+
+.submit-btn {
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
